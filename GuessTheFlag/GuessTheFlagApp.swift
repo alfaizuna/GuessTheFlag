e6167c18f0303675297bc5eab6247b5006839add
alfaizuna <alfaizunaulia@gmail.com>
2024-09-08 13:54:22 +0700

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alfaizuna Aulia Ulloh on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
